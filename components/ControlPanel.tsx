914adfc6344c6612b38fbffff36f5336a3e3e5f5
Yusuf Canbaz <yusufsaidcanbaz@gmail.com>
2025-10-19 22:52:42 +0300

    feat: Introduce reusable Checkbox component
    Refactors the control panel to use a new, generic Checkbox component.

--- components/ControlPanel.tsx
+++ components/ControlPanel.tsx
@@ -3,6 +3,7 @@ import { CropParams } from '../types';
 import { Button } from './Button';
 import { WandIcon, ResetIcon, DownloadIcon, RotateIcon, CropIcon, TrashIcon } from './icons';
 import { useTranslation } from '../hooks/useTranslation';
+import { Checkbox } from './Checkbox';
 
 interface ControlPanelProps {
   rotation: number;
@@ -150,30 +151,12 @@ export const ControlPanel: React.FC<ControlPanelProps> = ({
         </InputGroup>
 
         <div>
-          <label className="flex items-center space-x-3 cursor-pointer">
-            <div className="relative">
-              <input
-                type="checkbox"
-                checked={keepCropperVertical}
-                onChange={(e) => setKeepCropperVertical(e.target.checked)}
-                className="peer appearance-none w-5 h-5 bg-gray-700 border-2 border-gray-600 rounded-md checked:bg-indigo-600 checked:border-transparent focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-indigo-500 transition cursor-pointer"
-              />
-              <svg
-                className="absolute w-3.5 h-3.5 text-white pointer-events-none top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 opacity-0 peer-checked:opacity-100 transition-opacity"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-                aria-hidden="true"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-                  clipRule="evenodd"
-                />
-              </svg>
-            </div>
-            <span className="text-sm font-medium text-gray-300 select-none">{t('controls.keepVertical')}</span>
-          </label>
+          <Checkbox
+            id="keep-cropper-vertical"
+            checked={keepCropperVertical}
+            onChange={(e) => setKeepCropperVertical(e.target.checked)}
+            label={t('controls.keepVertical')}
+          />
         </div>
 
       </div>
