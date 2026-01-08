b70cf60973d62efea562880b60e15cdd258b60f1
canbax <yusufsaidcanbaz@gmail.com>
2026-01-08 23:21:27 +0300

    use compressed logo image

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 export const Logo: React.FC<{ className?: string }> = ({ className = "h-8 w-8" }) => (
   <img
+    loading="lazy"
     src="/favicon.ico"
     alt="AutoFix Image Logo"
     className={className}
